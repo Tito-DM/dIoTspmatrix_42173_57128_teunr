5732d44efbda78289356f06d47bcf1df8c8b98ce
pyong <muanda1993@hotmail.com>
2022-05-29 22:48:31 +0100

    modify row func

--- MatrixSparseDOK.py
+++ MatrixSparseDOK.py
@@ -74,7 +74,7 @@ class MatrixSparseDOK(MatrixSparse):
     def __len__(self) -> int:
         return len(self._items)
 
-    def _add_number(self, other: tuple[int, float]) -> Matrix:
+    def _add_number(self,pos: tuple[Position,position] ,val: tuple[int, float]) -> Matrix:
         if isinstance(val,(float,int)) and isinstance(pos,(Position,tuple)):
             if isinstance(pos, Position):
                 if val != self.zero:
@@ -90,91 +90,69 @@ class MatrixSparseDOK(MatrixSparse):
                 raise ValueError(self.MSG_setter)
         else:
             raise ValueError(self.MSG_setter)
-
-    def _add_matrix(self, other: MatrixSparse) -> MatrixSparse:
-        dim1 = self.dim()
-        dim2 = other.dim()
-        dim1_length = (dim1[1][1] - dim1[0][1]) + 1
-        dim1_height = (dim1[1][0] - dim1[0][0]) + 1
-        dim2_length = (dim2[1][1] - dim2[0][1]) + 1
-        dim2_height = (dim2[1][0] - dim2[0][0]) + 1
-
-        if((dim1_length == dim2_length) and (dim1_height == dim2_height) and self.zero == other.zero):
-            aux = MatrixSparseDOK(self.zero)
-            for x in range(dim1_height):
-                for y in range(dim1_length):                 
-                    if (self[x + dim1[0][0], y + dim1[0][1]] == self.zero):
-                        pass
+    
+    def _add_matrix_auxl(dim_x_length,dim_y_length,dim_x_height,dim_y_height,self,other,dim_x,dim_y) -> MatrixSparse:
+        if((dim_x_length == dim_y_length) and (dim_x_height == dim_y_height) and self.zero == other.zero):
+            spmax = MatrixSparseDOK(self.zero)
+            for x in range(dim_x_height):
+                for y in range(dim_x_length):                 
+                    if (self[x + dim_x[0][0], y + dim_x[0][1]] == self.zero):
+                        if (other[x + dim_y[0][0], y + dim_y[0][1]] != self.zero):
+                            spmax[x + dim_x[0][0], y + dim_x[0][1]] = other[x + dim_y[0][0], y + dim_y[0][1]]
                     else:
-                        aux[x + dim1[0][0], y + dim1[0][1]] = self[x + dim1[0][0], y + dim1[0][1]]
+                        spmax[x + dim_x[0][0], y + dim_x[0][1]] = self[x + dim_x[0][0], y + dim_x[0][1]]
 
-                    if (other[x + dim2[0][0], y + dim2[0][1]] == other.zero):
-                        pass
-                    elif aux[x + dim2[0][0], y + dim2[0][1]] != other.zero:
-                        aux[x + dim2[0][0], y + dim2[0][1]] += other[x + dim2[0][0], y + dim2[0][1]]
+                    if (other[x + dim_y[0][0], y + dim_y[0][1]] == other.zero):
+                        if (self[x + dim_x[0][0], y + dim_x[0][1]] != self.zero):
+                            spmax[x + dim_x[0][0], y + dim_x[0][1]] += self[x + dim_x[0][0], y + dim_x[0][1]]
+                    elif spmax[x + dim_y[0][0], y + dim_y[0][1]] != other.zero:
+                        spmax[x + dim_y[0][0], y + dim_y[0][1]] += other[x + dim_y[0][0], y + dim_y[0][1]]
                     else:
-                        aux[x + dim2[0][0], y + dim2[0][1]] = other[x + dim2[0][0], y + dim2[0][1]]
-
-
-
-            return aux
+                        spmax[x + dim_y[0][0], y + dim_y[0][1]] = other[x + dim_y[0][0], y + dim_y[0][1]]
+            return spmax
         else:
             raise ValueError("_add_matrix() incompatible matrices")
 
+    def _add_matrix(self, other: MatrixSparse) -> MatrixSparse:
+        dim_x= self.dim()
+        dim_y = other.dim()
+        dim_x_length = (dim_x[1][1] - dim_x[0][1]) + 1
+        dim_x_height = (dim_x[1][0] - dim_x[0][0]) + 1
+        dim_y_length = (dim_y[1][1] - dim_y[0][1]) + 1
+        dim_y_height = (dim_y[1][0] - dim_y[0][0]) + 1
+        _add_matrix_auxl(dim_x_length,dim_y_length,dim_x_height,dim_y_height,self,other,dim_x,dim_y)
+
     def _mul_number(self, other: tuple[int, float]) -> Matrix:
         if isinstance(other, (int, float)):
-            aux = self.__copy__()
+            spmax_copy = self.__copy__()
             for key in self:
-                aux[key] *= other
-            return aux
+                spmax_copy[key] *= other
+            return spmax_copy
 
     def _mul_matrix(self, other: MatrixSparse) -> MatrixSparse:
-        dim1 = self.dim()
-        dim2 = other.dim()
-        dim1_length = (dim1[1][1] - dim1[0][1]) + 1
-        dim1_height = (dim1[1][0] - dim1[0][0]) + 1
-        dim2_length = (dim2[1][1] - dim2[0][1]) + 1
-        dim2_height = (dim2[1][0] - dim2[0][0]) + 1
-        
-        max_row_1,max_col_1 = dim1[1]
-        max_row_2,max_col_2 = dim2[1]
 
-        min_row_1,min_col_1 = dim1[0]
-        min_row_2,min_col_2 = dim2[0]
+        dim_x = self.dim()
+        dim_y = other.dim()
+        dim_x_length = (dim_x[1][1] - dim_x[0][1]) + 1
+        dim_y_height = (dim_y[1][0] - dim_y[0][0]) + 1
+        max_row_x,max_col_x = dim_x[1]
+        max_row_y,max_col_y = dim_y[1]
+        min_row_x,min_col_x= dim_x[0]
+        min_row_y,min_col_y = dim_y[0]
 
-        max_row_considered = max_row_1 if max_row_1 > max_row_2 else max_row_2
-        max_col_considered = max_col_1 if max_col_1 > max_col_2 else max_col_2
-
-        min_row_considered = min_row_1 if min_row_1 < min_row_2 else min_row_2
-        min_col_considered = min_col_1 if min_col_1 < min_col_2 else min_col_2
-
-
-
-        
-
-        if((dim1_length != dim2_height) or (self.zero != other.zero)):
-
-            raise ValueError("_mul_matrix() incompatible matrices")
-        else:
+        if((dim_x_length != dim_y_height) or (self.zero != other.zero)):
             result = MatrixSparseDOK(self.zero)
-            for x in range(min_row_1, max_row_1 + 1):
-                print("Linha: ",x)
-                for y in range(min_col_2, max_col_2 + 1):
-                    print("Coluna: ", y)
-                    aux = 0
-                    for k in range(dim1_length):
-                        if self[(x,k+min_col_1)] != self.zero and other[(k+min_row_2,y)] != other.zero:
-                            print(self[(x,k+min_col_1)],"*",other[(k+min_row_2,y)],"=",self[(x,k+min_col_1)]*other[(k+min_row_2,y)])
-                            aux += self[(x,k+min_col_1)]*other[(k+min_row_2,y)]
-                            print("Total: ", aux)
-                            print()
-                            # result[x + dim1[0][0]][y + dim1[0][0]] += self[x + dim1[0][0], k + dim1[0][1]] * other[k + dim1[0][0], y + dim1[0][1]]
+            for x in range(min_row_x, max_row_x + 1):
+                for y in range(min_col_y, min_col_y + 1):
+                    spmax = 0
+                    for k in range(dim_x_length):
+                        if self[(x,k+min_col_x)] != self.zero and other[(k+min_row_y,y)] != other.zero:
+                            spmax += self[(x,k+min_col_x)]*other[(k+min_row_y,y)]
                         else:
-                            print(self[(x,k+min_col_1)],"*",other[(k+min_row_2,y)],"=",self[(x,k+min_col_1)]*other[(k+min_row_2,y)])
                             continue
-                    result[(x,y)] = aux
-                    print(result)
-                print("Result intermédio: ")
+                    result[(x,y)] = spmax
+        else:
+              raise ValueError("_mul_matrix() incompatible matrices")
         return result
 
     def dim(self) -> tuple[Position, position]:
@@ -228,7 +206,8 @@ class MatrixSparseDOK(MatrixSparse):
                 for i in range(size):
                     spmax[Position(i,i)] = unitary
                 return spmax
-        raise ValueError("eye() invalid parameters")
+        else:
+            raise ValueError("eye() invalid parameters")
 
     def transpose(self) -> MatrixSparseDOK:
         spmax = MatrixSparseDOK(self.zero)
@@ -240,22 +219,16 @@ class MatrixSparseDOK(MatrixSparse):
 
     def compress(self) -> compressed:
         if self.sparsity() >= 0.5:
-            
             values = []
             indexes = []
             rows = [] 
             non_null_elem = []
-            
-            
             upper_left, bottom_right = self.dim()
             min_row,min_col = upper_left
             max_row,max_col = bottom_right
-
-           
             total_elem_row = max_col-min_col + 1
             total_rows = max_row-min_row + 1
             offsets = [0]*total_rows
-      
             rows = []
             aux = []
             for x in range(min_row,max_row+1):
@@ -263,8 +236,6 @@ class MatrixSparseDOK(MatrixSparse):
                 for y in range(min_col,max_col+1):
                     aux.append(self[Position(x,y)])
                 rows.append(aux)
-         
-            
             for row_num,row in enumerate(rows):
                 count = 0
                 for elem in row:
@@ -274,54 +245,36 @@ class MatrixSparseDOK(MatrixSparse):
             for row in rows:
                 print(row)
             rows = list(map(lambda x:(x[0],x[2]),sorted(non_null_elem, key = lambda x: x[1],reverse = True)))
-            print("checkar se rows ficou bem organizado")
-            for row in rows:
-                print(row)
-            print()
-            
             for c,aux in enumerate(rows):
                 row,row_num = aux
                 offset_idx = 0
                 value_idx = 0
                 row_elem_idx = 0
                 done = False
-                if bool(values):
-                    print("nao está vazio")
+                if (values):
                     while row_elem_idx < total_elem_row: 
                         if value_idx + offset_idx < len(values): 
                             if (values[value_idx+offset_idx] == self.zero or row[row_elem_idx] == self.zero): 
                                 value_idx += 1
                                 row_elem_idx += 1
-                                print("shift")
                             else:
-                                print("add offset")
                                 value_idx = 0
                                 row_elem_idx = 0
                                 offset_idx += 1
-                                print(offset_idx)
                         else:
                             break
                     for i,elem in enumerate(row):
                         if i + offset_idx < len(values):
-                            print("adicionar")
-                            indexes[i+offset_idx] = row_num if elem != self.zero else indexes[i+offset_idx] # tem que se -1 para nao se confundir com 0
+                            indexes[i+offset_idx] = row_num if elem != self.zero else indexes[i+offset_idx]
                             values[i+offset_idx] = elem if elem != self.zero else values[i+offset_idx]   
                         else:
                             indexes.append(row_num if elem != self.zero else -1)
                             values.append(elem)
                         offsets[row_num-min_row] = offset_idx                           
                 else:
-                    print("está vazio: ")
-                    # definir vetores value e indexes com 
                     values = row
-                    indexes = list(map(lambda x: row_num if x != self.zero else -1,row)) # tem que se -1 para nao se confundir com 0
+                    indexes = list(map(lambda x: row_num if x != self.zero else -1,row)) 
                     offsets[0] = offset_idx
-                    print("Novo loop: ", row_num)
-                    print("values: ",values)
-                    print("indexes: ",indexes)
-                    print("offests: ",offsets)
-                    print("row: ",row)
-                    print()
             return ((min_row,min_col), self.zero, tuple(values), tuple(indexes), tuple(offsets))
         raise ValueError("compress() dense matrix")
     
