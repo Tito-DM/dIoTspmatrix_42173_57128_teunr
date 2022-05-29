d5387138f26a897a600d1b25bb8b303e32512e21
Tito <muanda1993@hotmail.com>
2022-05-29 23:09:32 +0100

    Update MatrixSparse.py

--- MatrixSparse.py
+++ MatrixSparse.py
@@ -1,30 +1,46 @@
 from __future__ import annotations
 from Matrix import *
+from typing import Tuple
+
 
 position = Tuple[int, int]
-#compressed = Tuple[position, float, tuple[float, ...], tuple[int], ..., tuple[int, ...]]
-
+compressed = Tuple[position, float, Tuple[float], Tuple[int], Tuple[int]]
 
 class MatrixSparse(Matrix):
     _zero = float
 
     def __init__(self, zero):
-        pass
+        self._zero = zero
 
     @property
     def zero(self) -> float:
-        pass
+        return self._zero
 
+    #TODO: Ver como funciona
     @zero.setter
     def zero(self, val: float):
-        pass
+        if isinstance(val,(int,float)):
+            self._zero = float(val)
+        for key in self:
+            if(self[key] == self._zero):
+                del self._items[key]
+
+                
 
     @abstractmethod
     def __len__(self) -> int:
         raise NotImplementedError
 
     def sparsity(self) -> float:
-        pass
+        pos = self.dim()
+        if (self._items):
+            min_r,min_c = pos[0]
+            max_r,max_c = pos[1]
+            total = (max_c-min_c+1)*(max_r-min_r+1)
+            zero = total - len(self._items)
+            return zero/float(total)
+        else:
+            return 1.0
 
     @staticmethod
     @abstractmethod
